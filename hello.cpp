6092db136ea87e0e082ffca3a0b103432070cf30
EkaterinaMalasay <ekaterina.malasay@gmail.com>
2019-07-01 10:33:55 +0000

    Canny filter

--- hello.cpp
+++ hello.cpp
@@ -1,3 +1,5 @@
+// Example 2-10. The same object can load videos from a camera or a file
+// 
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
@@ -14,25 +16,44 @@ void help(char** argv ) {
 }
     
 
-int main() {
+int main( int argc, char** argv ) {
+    
+    help(argv);
 
-    cv::Mat image(cv::Size(256,256), CV_8UC3);
 
-    for (int x = 0; x < image.size().width; ++x) {
-	for (int y = 0; y <image.size().height; ++y) {
-	    image.at < cv::Vec3b > (y,x)[0] = x;
-	    image.at < cv::Vec3b > (y,x)[1] = y;
-	    image.at < cv::Vec3b > (y,x)[2] = (x+y)/2;
-	}
-    }
 
-    cv::namedWindow( "Example", cv::WINDOW_AUTOSIZE );
+  cv::namedWindow( "Example 2-10", cv::WINDOW_AUTOSIZE );
+  cv::VideoCapture cap;
+
+  if (argc==1) {
+    cap.open(0); // open the first camera
+  } else {
+    cap.open(argv[1]);
+  }
+
+  if( !cap.isOpened() ) { // check if we succeeded
+    std::cerr << "Couldn't open capture." << std::endl;
+    return -1;
+  }
+
+  cv::Mat frame;
+cv::Mat gray_image;
+cv::Mat edge, draw;
 
   for(;;) {
-    if( image.empty() ) break; // Ran out of film
-    cv::imshow( "Example", image );
-    if( (char) cv::waitKey(33) >= 0 ) break;
-    }
 
-    return 0;
+    cap >> frame;
+cv::cvtColor(frame, gray_image, cv::COLOR_BGR2GRAY);
+cv::Canny(gray_image, edge, 50,150,3);
+
+    if( frame.empty() ) break; // Ran out of film
+
+    cv::imshow( "Example 2-10", edge );
+
+    if( (char) cv::waitKey(33) >= 0 ) break;
+
+  }
+
+  return 0;
+
 }
